39ce1c6fc776313ebb01c78d2b601a8e766bae27
Ben Zhang <bzhang@groupon.com>
2014-06-19 16:48:51 -0700

    fix library loading

--- lib/metrics_logger.rb
+++ lib/metrics_logger.rb
@@ -1,6 +1,10 @@
 require "metrics_logger/version"
 require "metrics_logger/configuration"
 require "metrics_logger/metrics_logger"
+require 'thread_safe'
+require 'timecop'
+require 'faraday'
+require 'json'
 
 module MetricsLogger
   def self.method_missing(method, *args, &block)
